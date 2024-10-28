88fbccc8a0301e430c16d3f007d62cb73e1d7531
salasensorial <sala.sensorial.alece@gmail.com>
2024-10-28 11:56:24 +0000

    atualizacao docker

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Execute os comandos de cache do Artisan após iniciar o container
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Inicie o PHP-FPM
-php-fpm
